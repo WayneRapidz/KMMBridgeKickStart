bfd5f7d21d5280d10703e45f396e92deb7f3f0bb
Wayne Lee <wayne.lee@rapidz.io>
2023-03-15 02:21:43 +0800

    v0.2.6

--- breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/CryptoUtils.kt
+++ breeds/src/commonMain/kotlin/co/touchlab/kmmbridgekickstart/CryptoUtils.kt
@@ -2,11 +2,8 @@ package co.touchlab.kmmbridgekickstart
 
 import co.touchlab.kmmbridgekickstart.Constants.MIN_LENGTH_CRYPTO_ADDRESS
 
-object CryptoAddressFactory {
-    fun createCryptoAddressValidator() = CryptoAddressValidator
-}
 
-object CryptoAddressValidator{
+class CryptoUtils{
     fun isCryptoAddressValid(cryptoAddress: String): Boolean{
         return !cryptoAddress.isNullOrEmpty() && cryptoAddress.length > MIN_LENGTH_CRYPTO_ADDRESS
     }
