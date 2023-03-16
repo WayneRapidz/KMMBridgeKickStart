f30bda23d1906839c8867cfa102b68f50b427d06
runner <runner@Mac-1678951793287.local>
2023-03-16 07:41:38 +0000

    KMM SPM package release for 0.2.11

--- Package.swift
+++ Package.swift
@@ -1,6 +1,8 @@
 // swift-tools-version:5.3
 import PackageDescription
 
+let remoteKotlinUrl = "https://maven.pkg.github.com/WayneRapidz/KMMBridgeKickStart/io/rapidz/kmmbridgekickstart/allshared-kmmbridge/0.2.11/allshared-kmmbridge-0.2.11.zip"
+let remoteKotlinChecksum = "13c79bff9b8feefd5bed0265a76f3094770aebb1449ea0e08c90f3e86445f43f"
 let packageName = "allshared"
 
 let package = Package(
@@ -17,7 +19,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: packageName,
-            path: "./allshared/build/XCFrameworks/debug/\(packageName).xcframework"
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
         )
         ,
     ]
